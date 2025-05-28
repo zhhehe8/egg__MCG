4afdcc074de9aeec29f7b4a90274676deb870389
zhhehe8 <zhhehe8@163.com>
2025-05-28 11:31:38 +0800

    Create egg_time.py

---
+++ egg_time.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+"""Fig2 x,y轴原始信号、滤波信号及R峰展示图"""
+
+# 1. 加载数据
+data = np.loadtxt(r'C:\Users\Xiaoning Tan\Desktop\egg_2025\B_egg\B_egg_d20\egg_d20_B30_t1_待破壳.txt', skiprows=2, encoding="utf-8")
+Bx = data[:, 0]
+By = data[:, 1]
+fs = 1000  # 采样率
+
+# 2. 检测参数设置
+
+""" 设置滤波器参数 """
+filter_order_bandpass = 4  # 带通滤波器的阶数 (根据用户最新提供)
+lowcut_freq = 0.5          # Hz, 低截止频率
+highcut_freq = 45.0        # Hz, 高截止频率
+notch_freq_mains = 50.0    # Hz, 工频干扰频率
+Q_factor_notch = 30.0      # 陷波滤波器的品质因数
+
+""" 设置R峰检测参数 """
+R_peak_min_height_factor = 0.4 
+R_peak_min_distance_ms = 150   
+
+
+
